2edda365dd3dbe1a5cbfbd38164ea50a053352db
Alex Wolf <f.alexander.wolf@gmail.com>
2024-01-14 14:13:40 +0100

    ✨ Enable staging notebooks & code using the CLI (#20)

---
+++ tests/test_stage.py
@@ -0,0 +1,21 @@
+from lamin_cli._stage import decompose_url
+import subprocess
+
+
+def test_decompose_url():
+    url = "https://lamin.ai/laminlabs/arrayloader-benchmarks/record/core/Transform?uid=1GCKs8zLtkc85zKv"  # noqa
+    instance_identifier, entity, uid = decompose_url(url)
+    assert instance_identifier == "laminlabs/arrayloader-benchmarks"
+    assert entity == "transform"
+    assert uid == "1GCKs8zLtkc85zKv"
+
+
+def test_stage():
+    result = subprocess.run(
+        "lamin stage"
+        " 'https://lamin.ai/laminlabs/arrayloader-benchmarks/record/core/Transform?uid=1GCKs8zLtkc85zKv'",  # noqa
+        shell=True,
+        capture_output=True,
+    )
+    print(result)
+    assert result.returncode == 0
