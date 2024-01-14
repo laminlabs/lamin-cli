b36a6fb027e87dd45c22206fb769efc549a3e0c1
Alex Wolf <f.alexander.wolf@gmail.com>
2024-01-14 11:36:27 +0100

    ✅ More tests

--- lamin_cli/_stage.py
+++ lamin_cli/_stage.py
@@ -2,24 +2,28 @@ from typing import Tuple
 from lamin_utils import logger
 
 
-def decompose_url(url: str) -> Tuple[str, str]:
+def decompose_url(url: str) -> Tuple[str, str, str]:
     assert "Transform" in url
     uid = url.split("uid=")[1]
-    return "transform", uid
+    instance_identifier = "/".join(url.replace("https://lamin.ai/", "").split("/")[:2])
+    return instance_identifier, "transform", uid
 
 
 def stage(identifier: str):
-    if identifier.startswith("https://lamin.ai"):
-        entity, uid = decompose_url(identifier)
-    else:
-        entity, uid = identifier.split()
     import lamindb as ln
 
+    ln.settings.verbosity = "success"
+    if identifier.startswith("https://lamin.ai"):
+        instance_identifier, entity, uid = decompose_url(identifier)
+    else:
+        entity, uid = identifier.split()
+        instance_identifier = ln.setup.settings.instance.identifier
+
     if entity == "transform":
-        transform = ln.Transform.filter(uid=uid).one()
+        transform = ln.Transform.using(instance_identifier).filter(uid=uid).one()
         filepath_cache = transform.source_code.stage()
         target_filename = f"{transform.short_name}.ipynb"
         filepath_cache.rename(target_filename)
-        logger.success(f"staged source code of transform {uid}")
+        logger.success(f"staged source code of transform {uid} as {target_filename}")
     else:
         raise NotImplementedError
--- tests/test_stage.py
+++ tests/test_stage.py
@@ -1,4 +1,5 @@
 from lamin_cli._stage import decompose_url
+import subprocess
 
 
 def test_decompose_url():
@@ -6,3 +7,14 @@ def test_decompose_url():
     entity, uid = decompose_url(url)
     assert entity == "transform"
     assert uid == "1GCKs8zLtkc85zKv"
+
+
+def test_stage():
+    result = subprocess.run(
+        "lamin stage"
+        " 'https://lamin.ai/laminlabs/arrayloader-benchmarks/record/core/Transform?uid=1GCKs8zLtkc85zKv'",  # noqa
+        shell=True,
+        capture_output=True,
+    )
+    print(result)
+    assert result.returncode == 0
