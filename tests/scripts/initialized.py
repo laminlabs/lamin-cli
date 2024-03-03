d309d4eb8c8115357e9827e03b241871387b29c6
Alex Wolf <f.alexander.wolf@gmail.com>
2024-03-03 17:49:53 +0100

    ✅ Test re-running a script (#31)

--- tests/scripts/initialized.py
+++ tests/scripts/initialized.py
@@ -6,6 +6,9 @@ ln.transform.version = "1"
 
 
 if __name__ == "__main__":
-    ln.track()
+    # we're using new_run here to mock the notebook situation
+    # and cover the look up of an existing run in the tests
+    # new_run = True is trivial
+    ln.track(new_run=False)
 
     print("hello!")
