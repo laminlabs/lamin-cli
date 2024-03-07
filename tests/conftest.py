9374d1abeb404320c55acdaafd78dbc2ee3a3546
Alex Wolf <f.alexander.wolf@gmail.com>
2024-03-07 19:47:33 +0100

    💚 Fix
    '

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,18 @@
 import shutil
 
-import lamindb_setup
+import lamindb as ln
 import pytest
 from lamin_utils import logger
 
 
 def pytest_sessionstart(session: pytest.Session):
-    lamindb_setup.init(
+    ln.setup.init(
         storage="./default_storage",
         name="lamindb-setup-notebook-tests",
     )
-    lamindb_setup._TESTING = True
 
 
 def pytest_sessionfinish(session: pytest.Session):
     logger.set_verbosity(1)
-    lamindb_setup.delete("lamindb-setup-notebook-tests", force=True)
+    ln.setup.delete("lamindb-setup-notebook-tests", force=True)
     shutil.rmtree("./default_storage")
