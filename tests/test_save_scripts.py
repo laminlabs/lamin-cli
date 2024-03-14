d0a8d6b6e86f5489ee59d8862d7e668d3c0f473c
Alex Wolf <f.alexander.wolf@gmail.com>
2024-03-14 21:28:49 +0100

    🔥 Remove lamin save for script

--- tests/test_save_scripts.py
+++ tests/test_save_scripts.py
@@ -34,30 +34,6 @@ def test_run_save_stage():
     assert "saved: Transform" in result.stdout.decode()
     assert "saved: Run" in result.stdout.decode()
 
-    # re-run the script
-    # python sub/lamin-cli/tests/scripts/initialized.py
-    result = subprocess.run(
-        f"python {filepath}",
-        shell=True,
-        capture_output=True,
-    )
-    print(result.stdout.decode())
-    assert result.returncode == 0
-    assert "loaded: Transform" in result.stdout.decode()
-    assert "loaded: Run" in result.stdout.decode()
-
-    # save the script
-    # lamin save sub/lamin-cli/tests/scripts/initialized.py
-    result = subprocess.run(
-        f"lamin save {filepath}",
-        shell=True,
-        capture_output=True,
-    )
-    print(result.stdout.decode())
-    assert result.returncode == 0
-    assert "saved transform" in result.stdout.decode()
-    assert filepath.exists()  # test that it's not cleaned out!
-
     # python sub/lamin-cli/tests/scripts/initialized.py
     # now, trying to run the same thing again will error
     result = subprocess.run(
@@ -67,6 +43,7 @@ def test_run_save_stage():
         env=env,
     )
     print(result.stdout.decode())
+    print(result.stderr.decode())
     assert result.returncode == 1
     assert "Please update your transform settings as follows" in result.stderr.decode()
 
