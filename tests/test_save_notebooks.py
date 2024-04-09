531d62be926c14a0a3de1f368c3025ac6b2abf74
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-04-09 11:33:51 +0200

    💚 Fix tests

--- tests/test_save_notebooks.py
+++ tests/test_save_notebooks.py
@@ -136,7 +136,7 @@ def test_save_consecutive():
     assert transform is not None
     assert transform.latest_report.path.exists()
     assert transform.latest_run.report.path == transform.latest_report.path
-    assert transform.source_code.hash == "ve5nSEbXgPmY9cx_QDf2cw"
+    assert transform.source_code.hash == "-hK6xaIrA04vJFAY2wWAQQ"
     assert transform.latest_run.environment.path.exists()
     assert transform.source_code.path.exists()
 
