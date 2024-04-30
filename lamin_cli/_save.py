42f6dda79be3bfb7485f6930da3b161a0e094869
Alex Wolf <f.alexander.wolf@gmail.com>
2024-04-30 04:55:32 +0200

    ♻️ Move get_stem_uid_and_version_from_file from lamindb here

--- lamin_cli/_save.py
+++ lamin_cli/_save.py
@@ -3,6 +3,44 @@ from pathlib import Path
 from typing import Optional, Union
 import lamindb_setup as ln_setup
 from lamin_utils import logger
+import re
+
+
+def get_stem_uid_and_version_from_file(file_path: Path) -> tuple[str, str]:
+    # line-by-line matching might be faster, but let's go with this for now
+    with open(file_path) as file:
+        content = file.read()
+
+    if file_path.suffix == ".py":
+        stem_uid_pattern = re.compile(
+            r'\.transform\.stem_uid\s*=\s*["\']([^"\']+)["\']'
+        )
+        version_pattern = re.compile(r'\.transform\.version\s*=\s*["\']([^"\']+)["\']')
+    elif file_path.suffix == ".ipynb":
+        stem_uid_pattern = re.compile(
+            r'\.transform\.stem_uid\s*=\s*\\["\']([^"\']+)\\["\']'
+        )
+        version_pattern = re.compile(
+            r'\.transform\.version\s*=\s*\\["\']([^"\']+)\\["\']'
+        )
+    else:
+        raise ValueError("Only .py and .ipynb files are supported.")
+
+    # Search for matches in the entire file content
+    stem_uid_match = stem_uid_pattern.search(content)
+    version_match = version_pattern.search(content)
+
+    # Extract values if matches are found
+    stem_uid = stem_uid_match.group(1) if stem_uid_match else None
+    version = version_match.group(1) if version_match else None
+
+    if stem_uid is None or version is None:
+        raise SystemExit(
+            "ln.settings.transform.stem_uid and ln.settings.transform.version aren't"
+            f" set in {file_path}\nCall ln.track() and copy/paste the output into the"
+            " notebook"
+        )
+    return stem_uid, version
 
 
 def save_from_filepath_cli(
@@ -17,7 +55,6 @@ def save_from_filepath_cli(
     ln_setup.settings.auto_connect = True
 
     import lamindb as ln
-    from lamindb.core._run_context import get_stem_uid_and_version_from_file
     from lamindb._finish import save_run_context_core
 
     ln_setup.settings.auto_connect = auto_connect_state
