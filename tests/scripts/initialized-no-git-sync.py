2090c555bf171307756ec2e4f86e12a8b404898e
Alex Wolf <f.alexander.wolf@gmail.com>
2024-04-30 03:10:49 +0200

    ♻️ Auto-connect

--- tests/scripts/initialized-no-git-sync.py
+++ tests/scripts/initialized-no-git-sync.py
@@ -1,9 +1,4 @@
-import lamindb as ln
-
-ln.connect("lamindb-unit-tests")
+import lamindb as ln  # noqa
 
 if __name__ == "__main__":
-    # we're using new_run here to mock the notebook situation
-    # and cover the look up of an existing run in the tests
-    # new_run = True is trivial
     print("hello!")
