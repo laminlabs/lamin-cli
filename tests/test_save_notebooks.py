6d75432b0e7a1c570b2a153f73db9a025cc2b58e
Alex Wolf <f.alexander.wolf@gmail.com>
2024-04-30 03:36:34 +0200

    💚 Fix hash

--- tests/test_save_notebooks.py
+++ tests/test_save_notebooks.py
@@ -99,7 +99,7 @@ def test_save_consecutive():
     assert transform is not None
     assert transform.latest_report.path.exists()
     assert transform.latest_run.report.path == transform.latest_report.path
-    assert transform.source_code.hash == "ur4ba9DSorUTYQ0WhP8G3Q"
+    assert transform.source_code.hash == "-tysu3DpnYTsS99C07ndVw"
     assert transform.latest_run.environment.path.exists()
     assert transform.source_code.path.exists()
 
