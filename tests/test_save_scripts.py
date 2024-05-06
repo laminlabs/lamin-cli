9649c54c73bb33079274ee65294b2731330c8460
Alex Wolf <f.alexander.wolf@gmail.com>
2024-05-06 17:39:12 +0200

    ♻️ Iterate

--- tests/test_save_scripts.py
+++ tests/test_save_scripts.py
@@ -11,10 +11,9 @@ scripts_dir = Path(__file__).parent.resolve() / "scripts"
 def test_run_save_cache():
     env = os.environ
     env["LAMIN_TESTING"] = "true"
-
     filepath = scripts_dir / "initialized.py"
+
     # attempt to save the script without it yet being run
-    # lamin save sub/lamin-cli/tests/scripts/initialized.py
     result = subprocess.run(
         f"lamin save {filepath}",
         shell=True,
@@ -58,6 +57,27 @@ def test_run_save_cache():
 
     # edit the script
     content = filepath.read_text() + "\n # edited"
+    filepath.write_text(content)
+
+    # re-run the script
+    result = subprocess.run(
+        f"python {filepath}",
+        shell=True,
+        capture_output=True,
+        env=env,
+    )
+    print(result.stdout.decode())
+    print(result.stderr.decode())
+    assert result.returncode == 1
+    assert "Did not find blob hash" in result.stderr.decode()
+
+    # edit the script to remove the git integration
+    content = filepath.read_text()
+    content_lines = content.split("\n")
+    content_lines.remove(
+        'ln.settings.sync_git_repo = "https://github.com/laminlabs/lamin-cli"'
+    )
+    content = "\n".join(content_lines)
     filepath.write_text(content)
 
     # re-run the script
