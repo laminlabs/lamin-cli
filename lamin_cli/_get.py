d7be0bd37a65065538bbd1e78753842150b5cae6
Alex Wolf <f.alexander.wolf@gmail.com>
2024-05-08 11:00:26 +0200

    ✨ Enable to get artifacts

--- lamin_cli/_get.py
+++ lamin_cli/_get.py
@@ -5,10 +5,13 @@ import lamindb_setup as ln_setup
 
 
 def decompose_url(url: str) -> Tuple[str, str, str]:
-    assert "transform" in url
-    uid = url.split("transform/")[1]
+    assert "transform" in url or "artifact" in url
+    for entity in ["transform", "artifact"]:
+        if entity in url:
+            break
+    uid = url.split(f"{entity}/")[1]
     instance_slug = "/".join(url.replace("https://lamin.ai/", "").split("/")[:2])
-    return instance_slug, "transform", uid
+    return instance_slug, entity, uid
 
 
 def get(url: str):
@@ -28,12 +31,14 @@ def get(url: str):
     ln.settings.verbosity = "success"
 
     if entity == "transform":
-        transform = ln.Transform.filter(uid=uid).one()
+        transform = ln.Transform.get(uid)
         filepath_cache = transform.source_code.cache()
         target_filename = transform.key
         if not target_filename.endswith(transform.source_code.suffix):
             target_filename += transform.source_code.suffix
         filepath_cache.rename(target_filename)
         logger.success(f"cached source code of transform {uid} as {target_filename}")
-    else:
-        raise NotImplementedError
+    elif entity == "artifact":
+        artifact = ln.Artifact.get(uid)
+        cache_path = artifact.cache()
+        logger.success(f"cached artifact {artifact} here:\n{cache_path}")
