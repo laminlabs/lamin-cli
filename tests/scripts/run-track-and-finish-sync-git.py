d811d67ed23318afdeb0ad834dae39bee6aa41e6
Alex Wolf <f.alexander.wolf@gmail.com>
2024-06-25 23:06:09 +0200

    ♻️ Set script name

--- tests/scripts/run-track-and-finish-sync-git.py
+++ tests/scripts/run-track-and-finish-sync-git.py
@@ -3,6 +3,8 @@ import lamindb as ln
 ln.settings.sync_git_repo = "https://github.com/laminlabs/lamin-cli"
 ln.settings.transform.stem_uid = "m5uCHTTpJnjQ"
 ln.settings.transform.version = "1"
+ln.settings.transform.name = "My good script"
+
 
 if __name__ == "__main__":
     # we're using new_run here to mock the notebook situation
