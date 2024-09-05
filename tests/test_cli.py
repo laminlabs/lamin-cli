d89551ac14e4ca3e7909a10bcb3f472e6fadc458
Koncopd <koncopd@gmail.com>
2024-09-05 12:12:53 +0200

    add test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,7 @@
 import os
+from datetime import datetime, timedelta, timezone
+from lamindb_setup.core._hub_client import connect_hub_with_auth
+from lamindb_setup.core._hub_core import create_api_key
 
 
 def test_entrypoint():
@@ -6,7 +9,7 @@ def test_entrypoint():
     assert exit_status == 0
 
 
-def test_login():
+def test_cli_login():
     exit_status = os.system("lamin login testuser1")
     assert exit_status == 0
 
@@ -14,3 +17,23 @@ def test_login():
         "lamin login testuser1 --key cEvcwMJFX4OwbsYVaMt2Os6GxxGgDUlBGILs2RyS"
     )
     assert exit_status == 0
+
+
+def test_cli_login_api_key():
+    expires_at = datetime.now(tz=timezone.utc) + timedelta(days=1)
+    api_key = create_api_key(
+        {
+            "expires_at": expires_at.strftime("%Y-%m-%d"),
+            "description": "test_cli_login_api_key",
+        }
+    )
+
+    os.environ["LAMIN_API_KEY"] = api_key
+    exit_status = os.system("lamin login")
+    assert exit_status == 0
+
+    hub = connect_hub_with_auth()
+    hub.table("api_key").delete().eq("description", "test_cli_login_api_key").execute()
+    hub.auth.sign_out({"scope": "local"})
+
+    os.system("lamin login testuser1@lamin.ai")
