679c6aa06ed15cb28196c6386bbc929b20c1683d
Alex Wolf <f.alexander.wolf@gmail.com>
2025-01-11 14:44:40 +0100

    💚 Fix

--- tests/test_migrate.py
+++ tests/test_migrate.py
@@ -10,28 +10,27 @@ def test_migrate_create():
     assert exit_status == 0
 
 
-# TODO
-# def test_migrate_deploy():
-#     os.system("lamin login testuser1")
-#     import lamindb as ln
+def test_migrate_deploy():
+    os.system("lamin login testuser1")
+    import lamindb as ln
 
-#     instance_slug = ln.setup.settings.instance.slug
-#     exit_status = os.system(
-#         "lamin connect testuser1/static-test-instance-private-sqlite"
-#     )
-#     assert exit_status == 0
-#     exit_status = os.system("lamin migrate deploy")
-#     assert exit_status == 0
-#     # now test that the hub got populated with the correct lamindb version
-#     # test it once we integrated it in the CLI output
-#     # instance = call_with_fallback_auth(
-#     #     sb_select_instance_by_id,
-#     #     instance_id=ln_setup.settings.instance.id.hex,
-#     # )
-#     # import lamindb
-#     # assert instance["lamindb_version"] == lamindb.__version__
-#     exit_status = os.system(f"lamin connect {instance_slug}")
-#     assert exit_status == 0
+    instance_slug = ln.setup.settings.instance.slug
+    exit_status = os.system(
+        "lamin connect testuser1/static-test-instance-private-sqlite"
+    )
+    assert exit_status == 0
+    exit_status = os.system("lamin migrate deploy")
+    assert exit_status == 0
+    # now test that the hub got populated with the correct lamindb version
+    # test it once we integrated it in the CLI output
+    # instance = call_with_fallback_auth(
+    #     sb_select_instance_by_id,
+    #     instance_id=ln_setup.settings.instance.id.hex,
+    # )
+    # import lamindb
+    # assert instance["lamindb_version"] == lamindb.__version__
+    exit_status = os.system(f"lamin connect {instance_slug}")
+    assert exit_status == 0
 
 
 # def test_migrate_squash():
