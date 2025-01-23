c51522aefc304d23762e1eff64898cd1e1f9d0d0
Lukas Heumos <lukas.heumos@posteo.net>
2025-01-23 19:40:57 +0100

    mb fix
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- lamin_cli/_save.py
+++ lamin_cli/_save.py
@@ -62,15 +62,15 @@ def save_from_filepath_cli(
     auto_connect_state = ln_setup.settings.auto_connect
     ln_setup.settings.auto_connect = True
 
-    if not ln_setup._check_instance_setup():
-        from lamindb_setup._check_setup import InstanceNotSetupError
-
-        raise ClickInstanceNotSetupError(InstanceNotSetupError.default_message)
-
     import lamindb as ln
     from lamindb._finish import save_context_core
 
     ln_setup.settings.auto_connect = auto_connect_state
+
+    if not ln_setup._check_instance_setup():
+        from lamindb_setup._check_setup import InstanceNotSetupError
+
+        raise ClickInstanceNotSetupError(InstanceNotSetupError.default_message)
 
     suffixes_transform = {
         "py": set([".py", ".ipynb"]),
