6d1a609e4e4295aea335798f45a26e4c92d284f4
Alex Wolf <f.alexander.wolf@gmail.com>
2025-04-07 22:02:40 +0200

    💚 Fix

--- lamin_cli/compute/modal.py
+++ lamin_cli/compute/modal.py
@@ -162,6 +162,6 @@ class Runner:
             image.pip_install(packages)
             .env(all_env_variables)
             .add_local_dir(local_dir, remote_dir)
-            .add_local_python_source("lamindb", "lamindb_setup")
             .run_commands("lamin settings set auto-connect true")
+            .add_local_python_source(["lamindb", "lamindb_setup"])
         )
