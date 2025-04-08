79f1fc7952659b6bcd2b546785bab034b0a579fa
Alex Wolf <f.alexander.wolf@gmail.com>
2025-04-08 12:28:02 +0200

    👷 Add CI

---
+++ noxfile.py
@@ -0,0 +1,13 @@
+import os
+
+import nox
+from laminci.nox import install_lamindb, login_testuser1
+
+IS_PR = os.getenv("GITHUB_EVENT_NAME") != "push"
+
+
+@nox.session
+def setup(session):
+    branch = "main" if IS_PR else "release"  # point back to "release"
+    install_lamindb(session, branch=branch)
+    login_testuser1(session)
--- tests/modal/test_modal.py
+++ tests/modal/test_modal.py
@@ -7,11 +7,13 @@ scripts_dir = Path(__file__).parent.parent.resolve() / "scripts"
 def test_run_on_modal():
     filepath = scripts_dir / "run-track-and-finish.py"
 
+    subprocess.run("lamin connect laminlabs/lamindata", shell=True, check=True)
     result = subprocess.run(
-        f"lamin run {filepath}",
+        f"lamin run {filepath} --project 1QLbS6N7wwiL",
         shell=True,
         capture_output=True,
     )
     print(result.stdout.decode())
     assert result.returncode == 0
-    assert "created Transform" in result.stdout.decode()
+    assert "hello!" in result.stdout.decode()
+    assert "finished Run" in result.stdout.decode()
