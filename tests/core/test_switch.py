f86114b97845df71a748c781df7bd29f5048c0a3
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-07 01:30:38 +0200

    💚 Fix

--- tests/core/test_switch.py
+++ tests/core/test_switch.py
@@ -6,10 +6,10 @@ import lamindb_setup as ln_setup
 def test_branch():
     exit_status = os.system("lamin switch --branch archive")
     assert exit_status == 0
-    assert ln_setup.settings.branch.uid == "a"
+    assert ln_setup.settings.branch.uid == 12 * "a"
     exit_status = os.system("lamin switch --branch main")
     assert exit_status == 0
-    assert ln_setup.settings.branch.uid == "m"
+    assert ln_setup.settings.branch.uid == 12 * "m"
 
 
 def test_space():
@@ -17,4 +17,4 @@ def test_space():
     assert exit_status == 256
     exit_status = os.system("lamin switch --space all")
     assert exit_status == 0
-    assert ln_setup.settings.space.uid == "a"
+    assert ln_setup.settings.space.uid == 12 * "a"
