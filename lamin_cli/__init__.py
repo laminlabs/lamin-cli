120e83c482b91b920b2891b61380f4ca5e3e540b
Alex Wolf <f.alexander.wolf@gmail.com>
2025-09-14 13:48:38 +0200

    🔖 Release 1.7.1

--- lamin_cli/__init__.py
+++ lamin_cli/__init__.py
@@ -5,7 +5,7 @@ This is the command line interface for interacting with LaminDB & LaminHub.
 The interface is defined in `__main__.py`. The root API here is used by LaminR to replicate the CLI functionality.
 """
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 from ._save import save
 
