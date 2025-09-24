6fc9a8b562dc3d287d8c21643386e19e1b8b73f2
Alex Wolf <f.alexander.wolf@gmail.com>
2025-09-24 16:19:00 +0200

    ♻️ Remaining functions

--- lamin_cli/__init__.py
+++ lamin_cli/__init__.py
@@ -7,9 +7,12 @@ The interface is defined in `__main__.py`. The root API here is used by LaminR t
 
 __version__ = "1.7.2"
 
+from lamindb_setup import disconnect, logout
 from lamindb_setup._connect_instance import _connect_cli as connect
 from lamindb_setup._init_instance import init
+from lamindb_setup._setup_user import login
 
+from ._delete import delete
 from ._save import save
 
-__all__ = ["save", "init", "connect"]
+__all__ = ["save", "init", "connect", "delete", "login", "disconnect"]
